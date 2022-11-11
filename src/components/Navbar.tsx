14a463d4e0d7a69aebc6830d36a5ebc32b9f23d1
TubesForLyfe <13520160@std.stei.itb.ac.id>
2022-11-11 13:30:35 +0700

    Feature: Navbar & Init Song Management

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
+import jwtDecode from 'jwt-decode'
+
 import './Navbar.css'
 
 const Navbar = () => {
+    const [cookies] = useCookies();
+
+    const getToken = () => {
+        if (cookies.binotify_premium_token) {
+            let decodedToken: any;
+            decodedToken = jwtDecode(cookies.binotify_premium_token);
+            return decodedToken.isAdmin;
+        }
+        return null;
+    }
+
     return (
         <nav className="navbar-container">
             <div className="profile">
                 <img src="https://www.pngitem.com/pimgs/m/108-1084833_spotify-icon-spotify-icon-white-png-transparent-png.png" className="navbar-image" />
             </div>
-            {/* <a className="nav-text" href="">Home</a> */}
+            {getToken() === true && <Link className="nav-text" to="add-premium-song">Daftar Permintaan Subscription</Link>}
+            {getToken() === false && <Link className="nav-text" to="add-premium-song">Tambah Lagu</Link>}
+            {getToken() === false && <Link className="nav-text" to="add-premium-song">Daftar Lagu</Link>}
         </nav>
     )
 }
--- src/pages/SongManagement/SongManagement.tsx
+++ src/pages/SongManagement/SongManagement.tsx
@@ -1,8 +1,45 @@
 import React from 'react'
 
+import Navbar from '../../components/Navbar'
+import './SongManagement.css'
+
 const SongManagement = () => {
     return (
-        <div>SongManagement</div>
+        <div className='page-container'>
+            <Navbar />
+            <div className="wrapper-detail-album">
+                <div className="album-detail-block-upper">
+                    <div className="album-detail-text">
+                        <p className="album-detail-title">Daftar Lagu</p>
+                    </div>
+                </div>
+
+                <div className="album-detail-block-bottomer">
+                    <table className="album-detail-songs-table">
+                    <tr>
+                        <th className="bg-17-17-17">#</th>
+                        <th className="bg-17-17-17">Title</th>
+                        <th className="album-detail-table-align-right bg-17-17-17">Singer</th>
+                        <th className="album-detail-table-align-right bg-17-17-17">Duration</th>
+                        <th className="album-detail-table-align-right bg-17-17-17">Detail</th>
+                    </tr>
+
+                    {/* <?php foreach($data["songs"] as $song): ?> */}
+                        <tr>
+                            <td className="bg-17-17-17">Nomor</td>
+                            <td className="bg-17-17-17">Judul</td>
+                            <td className="album-detail-table-align-right bg-17-17-17">Penyanyi</td>
+                            <td className="album-detail-table-align-right bg-17-17-17">Durasi</td>
+                            <td className="album-detail-table-align-right bg-17-17-17 album-detail-songs-buttons">
+                                <a href="<?php echo BASE_PUBLIC_URL">
+                                    <button className="album-detail-songs-button">Detail</button>
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
     )
 }
 

f8aa080bb16721d22db4b22775fc37ef2623863a
TubesForLyfe <13520160@std.stei.itb.ac.id>
2022-11-12 23:36:22 +0700

    Feature: List song complete

--- src/pages/SongManagement/SongManagement.tsx
+++ src/pages/SongManagement/SongManagement.tsx
@@ -6,13 +6,14 @@ import Navbar from '../../components/Navbar'
 import './SongManagement.css'
 
 const SongManagement = () => {
+    const [songs, setSongs] = useState([]);
     const [cookies] = useCookies();
 
     useEffect(() => {
         axios.get(`${import.meta.env.VITE_BINOTIFY_PREMIUM_API}/song`, {
             headers: {'Authorization': 'Bearer ' + cookies.binotify_premium_token}
         }).then(response => {
-            console.log(response);
+            setSongs(response.data);
         });
     }, []);
 
@@ -26,34 +27,39 @@ const SongManagement = () => {
                     </div>
                 </div>
 
-                <div className="album-detail-block-bottomer">
+                {songs.length > 0 && <div className="album-detail-block-bottomer">
                     <table className="album-detail-songs-table">
-                    <tr>
-                        <th className="bg-17-17-17">#</th>
-                        <th className="bg-17-17-17">Title</th>
-                        <th className="album-detail-table-align-right bg-17-17-17">Audio</th>
-                        <th className="album-detail-table-align-right bg-17-17-17">Edit</th>
-                        <th className="album-detail-table-align-right bg-17-17-17">Delete</th>
-                    </tr>
-
-                    {/* <?php foreach($data["songs"] as $song): ?> */}
                         <tr>
-                            <td className="bg-17-17-17">Nomor</td>
-                            <td className="bg-17-17-17">Judul</td>
-                            <td className="album-detail-table-align-right bg-17-17-17">Penyanyi</td>
-                            <td className="album-detail-table-align-right bg-17-17-17 album-detail-songs-buttons">
-                                <a href="<?php echo BASE_PUBLIC_URL">
-                                    <button className="album-detail-songs-button">Edit</button>
-                                </a>
-                            </td>
-                            <td className="album-detail-table-align-right bg-17-17-17 album-detail-songs-buttons">
-                                <a href="<?php echo BASE_PUBLIC_URL">
-                                    <button className="album-detail-songs-button">Delete</button>
-                                </a>
-                            </td>
+                            <th className="bg-17-17-17">#</th>
+                            <th className="bg-17-17-17">Judul</th>
+                            <th className="album-detail-table-align-right bg-17-17-17">Audio</th>
+                            <th className="album-detail-table-align-right bg-17-17-17">Edit</th>
+                            <th className="album-detail-table-align-right bg-17-17-17">Delete</th>
                         </tr>
+
+                        {songs.map((val: any, key: any) => {
+                            return (
+                                <tr>
+                                    <td className="bg-17-17-17">{key + 1}</td>
+                                    <td className="bg-17-17-17">{val.Judul}</td>
+                                    <td className="album-detail-table-align-right bg-17-17-17">
+                                        <audio controls src={`${import.meta.env.VITE_BINOTIFY_PREMIUM_API}/song/${val.Audio_path}`}></audio>
+                                    </td>
+                                    <td className="album-detail-table-align-right bg-17-17-17 album-detail-songs-buttons">
+                                        <a href="<?php echo BASE_PUBLIC_URL">
+                                            <button className="album-detail-songs-button">Edit</button>
+                                        </a>
+                                    </td>
+                                    <td className="album-detail-table-align-right bg-17-17-17 album-detail-songs-buttons">
+                                        <a href="<?php echo BASE_PUBLIC_URL">
+                                            <button className="album-detail-songs-button">Delete</button>
+                                        </a>
+                                    </td>
+                                </tr>
+                            )
+                        })}
                     </table>
-                </div>
+                </div>}
             </div>
         </div>
     )
